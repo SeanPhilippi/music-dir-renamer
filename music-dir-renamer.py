7f1ac1b987b3e14035188def43f600f09edfd9cc
SeanPhilippi <sean.philippi@gmail.com>
2023-02-05 15:28:15 -0500

    new_dirname values looking good mostly, need to think of fixes for a few errors, not renaming yet

--- music-dir-renamer.py
+++ music-dir-renamer.py
@@ -2,16 +2,46 @@
 # and to have a script I can run with a simple alias
 import os
 import eyed3
+from path import Path
+import argparse
 
-for dirpath, dirnames, filenames in os.walk("."):
-    for filename in filenames:
-        if filename.endswith(".mp3"):
-            filepath = os.path.join(dirpath, filename)
-            audiofile = eyed3.load(filepath)
-            artist = audiofile.tag.artist
-            album = audiofile.tag.album
-            year = audiofile.tag.getBestDate().year
-            new_dirname = "{} - {} ({})".format(artist, album, year)
-            new_dirpath = os.path.join(os.path.dirname(dirpath), new_dirname)
-            if not os.path.exists(new_dirpath):
-                os.rename(dirpath, new_dirpath)
+MUSIC_PATH = os.environ["MUSIC_PATH"]
+music_dir = Path(MUSIC_PATH)
+
+def music_dir_renamer():
+    for dirpath, dirnames, files in os.walk(music_dir):
+        for filename in files:
+            # print("==filename", filename)
+            if filename.endswith(".mp3"):
+                filepath = os.path.join(dirpath, filename)
+                # print("filepath", filepath)
+                audiofile = eyed3.load(filepath)
+                # print("audiofile", audiofile)
+                try:
+                    artist = audiofile.tag.artist
+                    # print("artist", artist)
+                except:
+                    # pass here because we don't want to rename if this is missing
+                    # will probably make the folder name even worse
+                    pass
+                try:
+                    album = audiofile.tag.album
+                    # print("album", album)
+                except:
+                    # pass here because we don't want to rename if this is missing
+                    # will probably make the folder name even worse
+                    pass
+                try:
+                    year = f" ({audiofile.tag.getBestDate().year})"
+                    # print("year", year)
+                except:
+                    year = ""
+                new_dirname = f"{artist} - {album}{year}"
+                print("--new_dirname", new_dirname)
+                new_dirpath = os.path.join(os.path.dirname(dirpath), new_dirname)
+                # print("--new_dirpath", new_dirpath)
+                break
+                # if not os.path.exists(new_dirpath):
+                      # os.rename(dirpath, new_dirpath)
+
+music_dir_renamer()                    
