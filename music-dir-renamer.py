6ea8b8c42284fc669c6ac60314ad0815e8e3bc9f
SeanPhilippi <sean.philippi@gmail.com>
2023-02-05 17:08:19 -0500

    seems to work, skipping dirs with None for artist or album or if there is an error

--- music-dir-renamer.py
+++ music-dir-renamer.py
@@ -7,41 +7,52 @@ import argparse
 
 MUSIC_PATH = os.environ["MUSIC_PATH"]
 music_dir = Path(MUSIC_PATH)
+# music_dir = Path("/home/kesto/Test")
 
 def music_dir_renamer():
     for dirpath, dirnames, files in os.walk(music_dir):
         for filename in files:
-            # print("==filename", filename)
             if filename.endswith(".mp3"):
                 filepath = os.path.join(dirpath, filename)
-                # print("filepath", filepath)
                 audiofile = eyed3.load(filepath)
-                # print("audiofile", audiofile)
                 try:
-                    artist = audiofile.tag.artist
-                    # print("artist", artist)
+                    if audiofile.tag.artist is not None:
+                        artist = audiofile.tag.artist
+                    else:
+                        print("!!! Skipped because artist was None")
+                        continue
                 except:
+                    print("!!! error when trying to get artist")
                     # pass here because we don't want to rename if this is missing
                     # will probably make the folder name even worse
-                    pass
+                    continue
                 try:
-                    album = audiofile.tag.album
-                    # print("album", album)
+                    if audiofile.tag.album is not None:
+                        album = audiofile.tag.album
+                    else:
+                        print("!!! Skipped because album was None")
+                        continue
                 except:
+                    print("!!! error when trying to get album")
                     # pass here because we don't want to rename if this is missing
                     # will probably make the folder name even worse
-                    pass
+                    continue
                 try:
                     year = f" ({audiofile.tag.getBestDate().year})"
-                    # print("year", year)
                 except:
                     year = ""
-                new_dirname = f"{artist} - {album}{year}"
-                print("--new_dirname", new_dirname)
+                # / can't be in a folder or file name
+                new_dirname = f"{artist} - {album}{year}".replace("/", "|")
                 new_dirpath = os.path.join(os.path.dirname(dirpath), new_dirname)
-                # print("--new_dirpath", new_dirpath)
+                print("--dirpath", dirpath)
+                try:
+                    if not os.path.exists(new_dirpath):
+                          os.rename(dirpath, new_dirpath)
+                except:
+                    print("==dirpath", dirpath)
+                    print("==new_dirpath", new_dirpath)
+                    print("!!! Error when trying to find path or rename")
+                    continue
                 break
-                # if not os.path.exists(new_dirpath):
-                      # os.rename(dirpath, new_dirpath)
 
 music_dir_renamer()                    
