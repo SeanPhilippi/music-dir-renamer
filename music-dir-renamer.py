4ec321d4f78a870ebf6f3ce0b6da57a5f77b5cf9
SeanPhilippi <sean.philippi@gmail.com>
2025-02-09 12:09:56 -0500

    added errors to printed error messages, also printing problematic folder and file paths

--- music-dir-renamer.py
+++ music-dir-renamer.py
@@ -5,36 +5,43 @@ import eyed3
 from path import Path
 
 MUSIC_PATH = os.environ["MUSIC_PATH"]
-music_dir = Path(MUSIC_PATH)
-# music_dir = Path("/home/kesto/Test")
+# music_dir = Path(MUSIC_PATH)
+music_dir = Path("/home/kesto/Test")
 # music_dir = Path("/media/kesto/My Passport/MUSIC")
 
-def rename_using_schema(audiofile, top_level=False):
+problematic_dirs = []
+
+def rename_using_schema(audiofile, dir_path):
     try:
         if audiofile.tag.artist is not None:
             artist = audiofile.tag.artist
         else:
             print("!!! Skipped because artist was None")
+            problematic_dirs.append(dir_path)
             return
-    except:
-        print("!!! error when trying to get artist")
+    except Exception as e:
+        print(f"!!! error when trying to get artist: {e}")
         # pass here because we don't want to rename if this is missing
         # will probably make the folder name even worse
+        problematic_dirs.append(dir_path)
         return
     try:
         if audiofile.tag.album is not None:
             album = audiofile.tag.album
         else:
             print("!!! Skipped because album was None")
+            problematic_dirs.append(dir_path)
             return
-    except:
-        print("!!! error when trying to get album")
+    except Exception as e:
+        print(f"!!! error when trying to get album: {e}")
         # pass here because we don't want to rename if this is missing
         # will probably make the folder name even worse
+        problematic_dirs.append(dir_path)
         return
     try:
         year = f" ({audiofile.tag.getBestDate().year})"
-    except:
+    except Exception as e:
+        print(f"!!! error trying to extract the year: {e}")
         year = ""
     # replace all invalid characters in filenames with '-'
     invalid_chars = "/|:\""
@@ -56,31 +63,32 @@ def music_dir_renamer():
                     filepath = path / filename
                     print(f"==filepath: {filepath}")
                     audiofile = eyed3.load(filepath)
-                    new_dirname = rename_using_schema(audiofile)
+                    new_dirname = rename_using_schema(audiofile, filename)
                     if new_dirname is not None:
                         # start renaming using new directory path
                         new_dirpath = Path(Path(dirpath).parent) / new_dirname
                         try:
                             if not Path(new_dirpath).exists():
                                 os.rename(dirpath, new_dirpath)
-                        except:
-                            print("==dirpath", dirpath)
-                            print("==new_dirpath", new_dirpath)
-                            print("!!! Error when trying to find path or rename")
+                        except Exception as e:
+                            print("==dirpath", repr(dirpath))
+                            print("==new_dirpath", repr(new_dirpath))
+                            print(f"!!! Error when trying to find path or rename: {e}")
                             continue
                     else:
                         print("!!! Skipped because could not create new directory name with metadata")
                     break
 
                 elif "INCOMPLETE" in filename:
-                    print("!!! Skipped because incomplete file")
+                    print(f"!!! Skipped {filename} because incomplete file")
 
         else: # if it's a file within top most directory (usually Music)
             for filename in files:
                 if filename.endswith(".mp3") and "INCOMPLETE" not in filename:
                     filepath = path / filename
+                    print(f"==filepath: {filepath}")
                     audiofile = eyed3.load(filepath)
-                    new_dirname = rename_using_schema(audiofile)
+                    new_dirname = rename_using_schema(audiofile, filename)
                     if new_dirname is not None:
                         new_dirpath = music_dir / new_dirname
                         if new_dirpath in dir_files_dict:
@@ -102,7 +110,14 @@ def music_dir_renamer():
             for filepath in filepaths:
                 # move the files to the new directory
                 filepath.rename(new_dirpath / filepath.name)
-        except:
-            print(f"!!! Error when trying to create directory or move file for {new_dirpath}")
+        except Exception as e:
+            print(f"!!! Error when trying to create directory or move file for {new_dirpath}: {e}")
+
+    if problematic_dirs:
+        print("problematic files and folders:")
+        for dir_path in problematic_dirs:
+            print(dir_path)
+    else:
+        print("No problematic folders!")
 
 music_dir_renamer()
